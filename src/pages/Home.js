c269ceac7db5a635dd914d27b03ea17ae83929ea
andreww136082 <andrewwang136082@gmail.com>
2022-06-20 16:34:01 -0700

    f

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,18 +22,33 @@ class Home extends Component {
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
     let new_list = [...this.state.todos, todo];
+    if(new_list.find(element => element === todo) = undefined)
+    {
     // Update the local state with the new array.
     this.setState({
       todos: new_list,
-    });
+    });}
+    else 
+    {
+      return;
+    }
   };
+  //deleteToDo removes an item from todo list once it is complete, which is indicated by user clicking on checkbox button
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+};
   render() {
     return (
       <div className="Home">
         <h1>Todo's </h1>
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
-        <AddTodo addTodo={this.addTodo} />
+        <AddTodo addTodo={this.addTodo} deleteTodo = {this.deleteTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
         <Todos todos={this.state.todos} />
